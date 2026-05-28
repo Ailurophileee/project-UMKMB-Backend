2e1b00ff7a795560833288b65740fe0c5ad21495
Salamah <fthira.sal@gmail.com>
2026-05-28 12:35:34 +0700

    bismillah

--- src/server/server-bersama.js
+++ src/server/server-bersama.js
@@ -4,7 +4,12 @@ import cors from 'cors';
 import ErrorHandler from '../middlewares/error.js';
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://project-umkmb-frontend.vercel.app', // atau gunakan '*' jika ingin bebas
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
